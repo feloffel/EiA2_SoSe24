4d4b7ddd8982432700b475d59441486c70b1530e
feloffel <95349435+feloffel@users.noreply.github.com>
2024-03-29 14:20:31 +0100

    L01_loesung

--- L01_Zufallsgedicht/script.js
+++ L01_Zufallsgedicht/script.js
@@ -1,18 +1,33 @@
 "use strict";
-//namespace
-const randomPoemArrays = {
-    // Arrays für Subjekte, Prädikate und Objekte
-    subjects: ['Harry', 'Hermine', 'Ron', 'Hagrid', 'Snape', 'Dumbledore'],
-    predicates: ['braut', 'liebt', 'studiert', 'hasst', 'zaubert', 'zerstört'],
-    objects: ['Zaubertränke', 'den Grimm', 'Lupin', 'Hogwarts', 'die Karte des Rumtreibers', 'Dementoren']
-};
-console.log(randomPoemArrays);
-for (let i = randomPoemArrays.subjects.length; i >= 1; i--) {
-    console.log(i);
-    const verse = getVerse([], [], []);
-    console.log(verse);
-}
-function getVerse(_subjects, _predicates, _objects) {
-    return "Alohomora";
-}
+var Zufallsgedicht;
+(function (Zufallsgedicht) {
+    let randomPoemArrays = {
+        // Arrays für Subjekte, Prädikate und Objekte
+        subjects: ['Kirk', 'Spock', 'McCoy', 'Picard', 'Data', 'Riker'],
+        predicates: ['befehligt', 'entdeckt', 'rettet', 'erforscht', 'bekämpft', 'analysiert'],
+        objects: ['die Enterprise', 'Ferengi', 'Klingonen', 'die Borg', 'das Holodeck', 'die Föderation']
+    };
+    // Schleife zur Erzeugung von Versen
+    for (let i = randomPoemArrays.subjects.length - 1; i >= 0; i--) {
+        let verse = getVerse(randomPoemArrays.subjects, randomPoemArrays.predicates, randomPoemArrays.objects);
+        console.log(verse);
+    }
+    // Funktion zur Erstellung eines Verses
+    function getVerse(_subjects, _predicates, _objects) {
+        // Variable zum Speichern des Verses
+        let verse = "";
+        // Zufällige Auswahl eines Subjekts, Prädikats und Objekts
+        let randomSubjectIndex = Math.floor(Math.random() * _subjects.length);
+        let subject = _subjects.splice(randomSubjectIndex, 1)[0];
+        verse += subject + " ";
+        let randomPredicateIndex = Math.floor(Math.random() * _predicates.length);
+        let predicate = _predicates.splice(randomPredicateIndex, 1)[0];
+        verse += predicate + " ";
+        let randomObjectIndex = Math.floor(Math.random() * _objects.length);
+        let object = _objects.splice(randomObjectIndex, 1)[0];
+        verse += object + ".";
+        // Rückgabe des Verses
+        return verse;
+    }
+})(Zufallsgedicht || (Zufallsgedicht = {}));
 //# sourceMappingURL=script.js.map
--- L01_Zufallsgedicht/script.ts
+++ L01_Zufallsgedicht/script.ts
@@ -1,21 +1,38 @@
-//namespace
-const randomPoemArrays = {
+namespace Zufallsgedicht {
+    let randomPoemArrays = {
     // Arrays für Subjekte, Prädikate und Objekte
-    subjects: ['Harry', 'Hermine', 'Ron', 'Hagrid', 'Snape', 'Dumbledore'],
-    predicates: ['braut', 'liebt', 'studiert', 'hasst', 'zaubert', 'zerstört'],
-    objects: ['Zaubertränke', 'den Grimm', 'Lupin', 'Hogwarts', 'die Karte des Rumtreibers', 'Dementoren']
-};
+    subjects:  ['Kirk', 'Spock', 'McCoy', 'Picard', 'Data', 'Riker'],
+    predicates: ['befehligt', 'entdeckt', 'rettet', 'erforscht', 'bekämpft', 'analysiert'],
+    objects: ['die Enterprise', 'Ferengi', 'Klingonen', 'die Borg', 'das Holodeck', 'die Föderation']
+    };
 
-console.log(randomPoemArrays)
+    // Schleife zur Erzeugung von Versen
+    for (let i: number = randomPoemArrays.subjects.length - 1; i >= 0; i--) {
+        let verse: string = getVerse(randomPoemArrays.subjects, randomPoemArrays.predicates, randomPoemArrays.objects);
+        console.log(verse); 
+    }
 
-for (let i = randomPoemArrays.subjects.length; i >= 1; i--) {
-    console.log(i);
-    const verse: string = getVerse([], [], []);
-    console.log(verse); 
+    // Funktion zur Erstellung eines Verses
+    function getVerse(_subjects: string[], _predicates: string[], _objects: string[]): string {
+        // Variable zum Speichern des Verses
+        let verse: string = "";
+
+        // Zufällige Auswahl eines Subjekts, Prädikats und Objekts
+        let randomSubjectIndex: number = Math.floor(Math.random() * _subjects.length);
+        let subject: string = _subjects.splice(randomSubjectIndex, 1)[0];
+        verse += subject + " ";
+
+        let randomPredicateIndex: number = Math.floor(Math.random() * _predicates.length);
+        let predicate: string = _predicates.splice(randomPredicateIndex, 1)[0];
+        verse += predicate + " ";
+
+        let randomObjectIndex: number = Math.floor(Math.random() * _objects.length);
+        let object: string = _objects.splice(randomObjectIndex, 1)[0];
+        verse += object + ".";
+
+        // Rückgabe des Verses
+        return verse;
+    }
 }
-
-function getVerse(_subjects: string[], _predicates: string[], _objects: string[]) {
-    return "Alohomora";
-  }
 
   
