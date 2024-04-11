1f7585d8ad28c870ba8d9a12a5348af119158929
feloffel <95349435+feloffel@users.noreply.github.com>
2024-04-11 22:30:42 +0200

    update

---
+++ L02_Event-Inspector/script.js
@@ -0,0 +1,22 @@
+"use strict";
+window.addEventListener('load', handleLoad);
+function handleLoad() {
+    // document.addEventListener('mousemove', handleMouseMove);
+    // document.addEventListener('click', handleClick);
+    // document.addEventListener('keyup', handleKeyup);
+    // document.querySelector('body')?.addEventListener('click', handleClick);
+    // document.querySelector('body')?.addEventListener('keyup', handleKeyup);
+    // document.querySelector('#div0')?.addEventListener('click', handleClick);
+    // document.querySelector('#div0')?.addEventListener('click', handleKeyup);
+    // document.querySelector('#div1')?.addEventListener('click', handleClick);
+    // document.querySelector('#div1')?.addEventListener('keyup', handleKeyup);
+}
+window.addEventListener('mousemove', setInfoBox);
+function setInfoBox(_event) {
+    let spanelement = document.querySelector('span');
+    spanelement.style.top = Number(_event.clientY + 5).toString() + "px";
+    spanelement.style.left = Number(_event.clientX + 5).toString() + "px";
+    spanelement.innerText = (_event.clientX + 5) + "px" + (_event.clientY + 5) + "px" + (_event.target + 5);
+    return spanelement;
+}
+//# sourceMappingURL=script.js.map
--- L02_Events_Vorbereitung/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var L02_Load;
-(function (L02_Load) {
-    document.addEventListener("load", handleLoad);
-    function handleLoad(_event) {
-        console.log(_event);
-    }
-})(L02_Load || (L02_Load = {}));
-//# sourceMappingURL=script.js.map
