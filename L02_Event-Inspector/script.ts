50065b7a0705db2fe888b24e2d4dbb3e3651bf5a
feloffel <95349435+feloffel@users.noreply.github.com>
2024-04-11 23:07:22 +0200

    L02

--- L02_Event-Inspector/script.ts
+++ L02_Event-Inspector/script.ts
@@ -1,17 +1,19 @@
 window.addEventListener('load', handleLoad);
 
-function handleLoad () {
-    // document.addEventListener('mousemove', handleMouseMove);
-    // document.addEventListener('click', handleClick);
-    // document.addEventListener('keyup', handleKeyup);
+function handleLoad (): void {
+    document.addEventListener('mousemove', setInfoBox);
+    document.addEventListener('click', logInfo);
+    document.addEventListener('keyup', logInfo);
  
-    // document.querySelector('body')?.addEventListener('click', handleClick);
-    // document.querySelector('body')?.addEventListener('keyup', handleKeyup);
+    document.querySelector('body')?.addEventListener('click', logInfo);
+    document.querySelector('body')?.addEventListener('keyup', logInfo);
 
-    // document.querySelector('#div0')?.addEventListener('click', handleClick);
-    // document.querySelector('#div0')?.addEventListener('click', handleKeyup);
-    // document.querySelector('#div1')?.addEventListener('click', handleClick);
-    // document.querySelector('#div1')?.addEventListener('keyup', handleKeyup);
+    document.querySelector('#div0')?.addEventListener('click', logInfo);
+    document.querySelector('#div0')?.addEventListener('click', logInfo);
+    document.querySelector('#div1')?.addEventListener('click', logInfo);
+    document.querySelector('#div1')?.addEventListener('keyup', logInfo);
+
+    
 }
 
 window.addEventListener('mousemove', setInfoBox);
@@ -20,8 +22,14 @@ function setInfoBox(_event:MouseEvent) {
     let spanelement: HTMLSpanElement = document.querySelector('span');
     spanelement.style.top = Number (_event.clientY + 5).toString() + "px";
     spanelement.style.left = Number (_event.clientX + 5).toString() + "px";
-    spanelement.innerText = (_event.clientX + 5) + "px" + (_event.clientY + 5) + "px" + (_event.target + 5);
+    spanelement.innerText = "Maus: " + (_event.clientX + 5) + "px " + (_event.clientY + 5) + "px " + "target: " + (_event.target);
     
     return spanelement;
-
 }
+
+function logInfo(_event: MouseEvent | KeyboardEvent): void {
+    console.log(_event);
+    console.log(_event.type);
+    console.log(_event.currentTarget);
+    console.log(_event.target);
+  }
